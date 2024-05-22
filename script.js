edc367bd52550b4888f38d894bcc343b9ea3f4e9
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-05-22 22:46:53 +0500

    mess commit

--- script.js
+++ script.js
@@ -6,27 +6,28 @@
  *
  */
 
-Array.prototype.myPush = function(...a) {
+Array.prototype.myPush = function (...a) {
   this.push(a[0]);
   init();
-}
+};
 
 const display = document.getElementById("fruits");
 const button = document.querySelector("#button");
 
-let fruits = ['mango', 'guava', 'apple', 'oragne'];
+let fruits = ["mango", "guava", "apple", "oragne"];
 
-const init = function() {
-  document.getElementById("fruits").innerHTML = '';
-  fruits.sort().forEach(fruit => {
+const init = function () {
+  document.getElementById("fruits").innerHTML = "";
+  fruits.sort().forEach((fruit) => {
     let item = document.createElement("li");
     item.textContent = fruit;
     document.getElementById("fruits").appendChild(item);
   });
-}
+};
 
-const addItem = function() {
-  fruits.myPush(document.getElementById('input').value);
-}
+const addItem = function () {
+  fruits.myPush(document.getElementById("input").value);
+  document.getElementById("input").value = "";
+};
 
 init();
