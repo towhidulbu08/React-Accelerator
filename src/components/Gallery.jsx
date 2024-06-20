a988ba47a90fa11b8a49ce46adb9f687bcb5b32e
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-06-20 07:07:39 +0500

    sentence added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,16 +1,15 @@
 function Profile() {
-    return (
-        <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
-    );
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
 }
 
 export default function Gallery() {
-    return (
-        <section>
-            <h1>Amazing scientists</h1>
-            <Profile />
-            <Profile />
-            <Profile />
-        </section>
-    );
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <h6> I AM A React Developer</h6>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
 }
