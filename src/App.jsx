113a4a19ce3f85952c41b8ff8f45698443130ecd
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-06-23 19:28:31 +0500

    note added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-const style1 = {
-    backgroundColor: "red",
-};
-export default function App() {
-    const style2 = {
-        backgroundColor: "green",
-    };
-
-    const color = "green";
-    let style = "";
-
-    if (color === "green") {
-        style = style1;
-    } else {
-        style = style2;
-    }
-
-    return (
-        <ul style={style}>
-            <li>Improve the videophone</li>
-            <li>Prepare aeronautics lectures</li>
-            <li>Work on the alcohol-fuelled engine</li>
-        </ul>
-    );
+export default function TodoList() {
+  return (
+    <ul
+      style={{
+        backgroundColor: "Violet",
+      }}
+    >
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
+  );
 }
