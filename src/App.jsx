98be4d7c1b3f2c6c5f8bcf1023803626ac0a5f3b
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-07-01 14:15:36 +0500

    L-2.6 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-import { createRoot } from "react-dom/client";
-import Image from "./data.js";
+import { useState } from "react";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<Image />);
+export default function App() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState("Hi!");
+  if (isSent) {
+    return <h1>Your message is on its way!</h1>;
+  }
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setIsSent(true);
+        sendMessage(message);
+      }}
+    >
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+function sendMessage(message) {
+  alert(message);
+}
