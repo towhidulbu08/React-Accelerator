baa6e043f2ae79246dff0f79e20afe889a84429d
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-07-08 16:01:55 +0500

    L.3.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import "./styles.css";
 const statuses = ["empty", "typing", "submitting", "success", "error"];
 
 function App() {
-    return (
-        <div>
-            {statuses.map((status) => (
-                <Form key={status} status={status} />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <h2>City quiz</h2>
+      <p>What city is located on two continents?</p>
+      {statuses.map((state) => (
+        <Form key={state} status={state} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
