77b786a4891e034258504eb7ba4ae7f544056ddd
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-07-11 10:04:22 +0500

    L-3.5 added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,40 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [inputs, setInputs] = useState([
-    {
-      id: 1,
-      class: "input",
-    },
-  ]);
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("typing");
 
-  function handleAddInput() {
-    setInputs([
-      ...inputs,
-      { id: inputs[inputs.length - 1].id + 1, class: "input" },
-    ]);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("sending");
+    await sendMessage(text);
+    setStatus("sent");
+  }
+
+  if (status === "sent") {
+    return <h1>Thanks for feedback!</h1>;
   }
 
   return (
-    <div>
-      {inputs.map((input) => (
-        <div style={{ marginBottom: "4px" }} key={input.id}>
-          <input type="text" label={input.class} />
-        </div>
-      ))}
-      <div style={{ marginTop: "20px" }}>
-        <button onClick={handleAddInput}>Add Input</button>
-      </div>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+        disabled={status === "sending"}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      <button disabled={status === "sending"} type="submit">
+        Send
+      </button>
+      {status === "sending" && <p>Sending...</p>}
+    </form>
   );
+}
+
+// Pretend to send a message.
+function sendMessage(text) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
 }
