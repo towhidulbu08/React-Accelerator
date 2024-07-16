df797d0783a6d46463d64b7b437b045f56568857
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-07-16 14:40:59 +0500

    L-3.11 Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import MyComponents from "./Components/MyComponents";
+import { useState } from "react";
+
+import Chat from "./Components/Chat.jsx";
+import ContactList from "./Components/ContactList.jsx";
+import contacts from "./Data/data.js";
 
 export default function App() {
+  const [to, setTo] = useState(contacts[0]);
   return (
-    <>
-      <MyComponents />
-    </>
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedContact={to}
+        onSelect={(contact) => setTo(contact)}
+      />
+      <Chat key={to.id} contact={to} />
+    </div>
   );
 }
---
+++ src/Data/data.js
@@ -0,0 +1,7 @@
+const contacts = [
+  { id: 0, name: "Taylor", email: "taylor@mail.com" },
+  { id: 1, name: "Alice", email: "alice@mail.com" },
+  { id: 2, name: "Bob", email: "bob@mail.com" },
+];
+
+export default contacts;
