ffc7d141fec1dc6d34db0ea5e4de93a4c7d76648
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-08-01 12:06:10 +0500

    L-4.1 added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,33 +1,33 @@
 import { useRef, useState } from "react";
 
 export default function Stopwatch() {
-    const [startTime, setStartTime] = useState(null);
-    const [now, setNow] = useState(null);
-    let intervalRef = useRef(null);
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const interValRef = useRef(null);
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+    // clearInterval(interValRef.current);
 
-    const handleStart = () => {
-        setStartTime(Date.now());
-        setNow(Date.now());
+    interValRef.current = setInterval(() => {
+      // Update the current time every 10ms.
+      setNow(Date.now());
+    }, 10);
+  }
+  function handleStop() {
+    clearInterval(interValRef.current);
+  }
 
-        intervalRef.current = setInterval(() => {
-            setNow(Date.now());
-        }, 10);
-    };
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
 
-    const handleStop = () => {
-        clearInterval(intervalRef.current);
-    };
-
-    let secondsPassed = 0;
-    if (startTime != null && now != null) {
-        secondsPassed = (now - startTime) / 1000;
-    }
-
-    return (
-        <>
-            <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
-        </>
-    );
+  return (
+    <>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>stop</button>
+    </>
+  );
 }
