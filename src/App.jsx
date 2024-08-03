94cf73703bf15f8eb81edcff40becad4aed4aba9
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-08-03 11:52:44 +0500

    L-4.8 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
-import VideoPlayer from "./components/VideoPlayer";
+import ChatRoom from "./components/ChatRoom";
 
 export default function App() {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [text, setText] = useState("");
-    return (
-        <>
-            <input value={text} onChange={(e) => setText(e.target.value)} />
-            <button onClick={() => setIsPlaying(!isPlaying)}>
-                {isPlaying ? "Pause" : "Play"}
-            </button>
-            <VideoPlayer
-                isPlaying={isPlaying}
-                src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-            />
-        </>
-    );
+  const [show, setShow] = useState(true);
+  return (
+    <>
+      {/* <MyComponent /> */}
+      <div>
+        <button onClick={() => setShow((s) => !s)}>
+          {show ? "Hide" : "Show"}
+        </button>
+      </div>
+      {show && <ChatRoom />}
+    </>
+  );
 }
