49f63081254ab4c1e8da3bf04f6607831d3884f3
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-08-22 17:36:39 +0500

    modified

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,26 +1,19 @@
 import { useEffect } from "react";
-import createConnection, { logVisit } from "../utils/connection";
+import createConnection from "../utils/connection";
 
 // const serverUrl = "https://localhost:1234";
 
 export default function ChatRoom({ roomId, serverUrl }) {
-    // console.log("rendering...");
-    useEffect(() => {
-        // synchoronize with external chat server
-        const connection = createConnection(serverUrl, roomId);
-        connection.connect();
-        // console.log(`Synchronozing with ${roomId}`);
+  useEffect(() => {
+    // synchoronize with external chat server
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
 
-        // cleanup
-        return () => {
-            // console.log(`Stop Synchronozing with ${roomId}`);
-            connection.disconnect();
-        };
-    }, [roomId, serverUrl]);
+    // cleanup
+    return () => {
+      connection.disconnect();
+    };
+  }, [roomId, serverUrl]);
 
-    useEffect(() => {
-        logVisit(roomId);
-    }, [roomId]);
-
-    return <h1>Welcome to the {roomId} room!</h1>;
+  return <h1>Welcome to the {roomId} room!</h1>;
 }
