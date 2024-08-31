0cdacd0f2710658ad558391c107d217adbe6e708
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-08-31 16:56:49 +0500

    L-4.28 added

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,35 @@ import ShowCount from "./components/ShowCount";
 import Title from "./components/Title";
 
 function App() {
-    const [count1, setCount1] = useState(0);
-    const [count2, setCount2] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-    const incrementByOne = useCallback(() => {
-        setCount1((prevCount) => prevCount + 1);
-    }, []);
+  const incrementByOne = useCallback(() => {
+    setCount1((prevCount) => prevCount + 1);
+  }, []);
 
-    const incrementByFive = useCallback(() => {
-        setCount2((prevCount) => prevCount + 5);
-    }, []);
+  const incrementByFive = useCallback(() => {
+    setCount2((prevCount) => prevCount + 5);
+  }, []);
 
-    const isEven = useMemo(() => {
-        let i = 0;
-        while (i < 1000000000) {
-            i += 1;
-        }
-
-        return count1 % 2 === 0;
-    }, [count1]);
-
-    return (
-        <div className="app">
-            <Title />
-            <ShowCount count={count1} title="Counter 1" />
-            <span>{isEven ? "Even" : "Odd"}</span>
-
-            <Button handleClick={incrementByOne}>Increment by one</Button>
-            <hr />
-            <ShowCount count={count2} title="Counter 2" />
-            <Button handleClick={incrementByFive}>Increment by five</Button>
-        </div>
-    );
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 1000000000) {
+      i += 1;
+    }
+    return count1 % 2 === 0;
+  }, [count1]);
+  return (
+    <div className="app">
+      <Title />
+      <span>{isEven ? `Even` : `Odd`}</span>
+      <ShowCount count={count1} title="Counter 1" />
+      <Button handleClick={incrementByOne}>Increment by one</Button>
+      <hr />
+      <ShowCount count={count2} title="Counter 2" />
+      <Button handleClick={incrementByFive}>Increment by five</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ShowCount.jsx
+++ src/components/ShowCount.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 function ShowCount({ count, title }) {
-    console.log(`rendering ${title}....`);
+  console.log(`rendering ${title}....`);
 
-    return (
-        <p>
-            {title} is {count}
-        </p>
-    );
+  return (
+    <p>
+      {title} is {count}
+    </p>
+  );
 }
 
 export default React.memo(ShowCount);
