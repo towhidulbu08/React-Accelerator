38fa66177a9d7ec8f704f4895f481d01d6c0377c
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-09-16 22:08:02 +0500

    L-5.2 added(Axios Crash Course)

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,112 @@
 import { useEffect, useState } from "react";
+import api from "./api/api.js";
 import "./App.css";
-import api from "./api/api";
 import AddPost from "./components/AddPost.jsx";
 import EditPost from "./components/EditPost.jsx";
 import Posts from "./components/Posts";
-// import initialPosts from "./data/db.js";
 
 export default function App() {
-    const [posts, setPosts] = useState([]);
-    const [post, setPost] = useState(null); // post I am editing
-    const [error, setError] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState(null); // post I am editing
+  const [error, setError] = useState(null);
 
-    const handleAddPost = async (newPost) => {
-        try {
-            const id = posts.length
-                ? Number(posts[posts.length - 1].id) + 1
-                : 1;
+  const handleAddPost = async (newPost) => {
+    try {
+      const id = posts.length ? Number(posts[posts.length - 1].id) + 1 : 1;
 
-            const finalPost = {
-                id: id.toString(),
-                ...newPost,
-            };
+      const finalPost = {
+        id: id.toString(),
+        ...newPost,
+      };
 
-            const response = await api.post("/posts", finalPost);
+      const response = await api.post("/posts", finalPost);
 
-            setPosts([...posts, response.data]);
-        } catch (err) {
-            setError(err.message);
+      // console.log(response);
+
+      setPosts([...posts, response.data]);
+    } catch (err) {
+      // network error, request did not reach to the server
+      setError(err.message);
+    }
+  };
+
+  const handleDeletePost = async (postId) => {
+    if (confirm("Are you sure you want to delete the post?")) {
+      try {
+        await api.delete(`/posts/${postId}`);
+        const newPosts = posts.filter((post) => post.id !== postId);
+        setPosts(newPosts);
+      } catch (err) {
+        // network error, request did not reach to the server
+        setError(err.message);
+      }
+    } else {
+      console.log("You chose not to delete the post!");
+    }
+  };
+
+  const handleEditPost = async (updatedPost) => {
+    try {
+      const response = await api.patch(`/posts/${updatedPost.id}`, updatedPost);
+      console.log(response);
+
+      const updatedPosts = posts.map((post) =>
+        post.id === response.data.id ? response.data : post
+      );
+
+      setPosts(updatedPosts);
+    } catch (err) {
+      // network error, request did not reach to the server
+      setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get("/posts");
+        console.log(response);
+
+        if (response && response.data) {
+          setPosts(response.data);
         }
+      } catch (err) {
+        setError(err.message);
+      }
     };
 
-    const handleDeletePost = async (postId) => {
-        if (confirm("Are you sure you want to delete the post?")) {
-            try {
-                await api.delete(`/posts/${postId}`);
-                const newPosts = posts.filter((post) => post.id !== postId);
-                setPosts(newPosts);
-            } catch (err) {
-                setError(err.message);
-            }
-        } else {
-            console.log("You chose not to delete the post!");
-        }
-    };
+    fetchPosts();
+  }, []);
 
-    const handleEditPost = async (updatedPost) => {
-        try {
-            const response = await api.patch(
-                `/posts/${updatedPost.id}`,
-                updatedPost
-            );
+  return (
+    <div>
+      <div>
+        <h1>API Request with api</h1>
+        <hr />
 
-            const updatedPosts = posts.map((post) =>
-                post.id === response.data.id ? response.data : post
-            );
-
-            setPosts(updatedPosts);
-        } catch (err) {
-            setError(err.message);
-        }
-    };
-
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await api.get("/posts");
-
-                if (response && response.data) {
-                    setPosts(response.data);
-                }
-            } catch (err) {
-                setError(err.message);
-            }
-        };
-
-        fetchPosts();
-    }, []);
-
-    return (
         <div>
-            <div>
-                <h1>API Request with Axios</h1>
-                <hr />
+          <Posts
+            posts={posts}
+            onDeletePost={handleDeletePost}
+            onEditClick={setPost}
+          />
 
-                <div>
-                    <Posts
-                        posts={posts}
-                        onDeletePost={handleDeletePost}
-                        onEditClick={setPost}
-                    />
+          <hr />
 
-                    <hr />
+          {!post ? (
+            <AddPost onAddPost={handleAddPost} />
+          ) : (
+            <EditPost post={post} onEditPost={handleEditPost} />
+          )}
 
-                    {!post ? (
-                        <AddPost onAddPost={handleAddPost} />
-                    ) : (
-                        <EditPost post={post} onEditPost={handleEditPost} />
-                    )}
-
-                    {error && (
-                        <>
-                            <hr />
-                            <div className="error">{error}</div>
-                        </>
-                    )}
-                </div>
-            </div>
+          {error && (
+            <>
+              <hr />
+              <div className="error">{error}</div>
+            </>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -1,35 +1,39 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8000",
 });
 
-const token = "jhdjfhdjfhdjfh";
+const token = "jdfjdjsjdjd";
 
-// request interceptor
+//request interceptor
 api.interceptors.request.use(
-    (config) => {
-        config.headers["Authorization"] = "Bearer " + token;
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
+  (config) => {
+    config.headers["Authorization"] = "Bearer " + token;
+    console.log(config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
-// response interceptor
-api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (err) => {
-        if (err.response) {
-            // error came from server
-            err.message = `Error from server: status: ${err.response.status} - message: ${err.response.statusText}`;
-        }
+//response interceptor
 
-        return Promise.reject(err);
+api.interceptors.response.use(
+  (response) => {
+    console.log(response);
+
+    return response;
+  },
+  (err) => {
+    if (err.response) {
+      // error came from server
+
+      err.message = `Error from server status ${err.response.status}- message:${err.response.statusText}`;
     }
+    return Promise.reject(err);
+  }
 );
 
 export default api;
