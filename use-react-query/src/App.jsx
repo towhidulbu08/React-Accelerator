bb1b4a06ea7d17000673a684315c53bfce5a9606
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-09-19 10:25:56 +0500

    L-5.3(React Query) added

--- use-react-query/src/App.jsx
+++ use-react-query/src/App.jsx
@@ -1,15 +1,23 @@
-import ProductList from "./components/ProductList"
-import ProductDetails from "./components/ProductDetails"
-import AddProduct from "./components/AddProduct"
-function App() {
+import { useState } from "react";
+import ProductDetails from "./components/ProductDetails";
 
+import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
+
+// import AddProduct from "./components/AddProduct"
+function App() {
+  const [passId, setPassId] = useState(9);
+
+  const handleShowDetails = (id) => {
+    setPassId(id);
+  };
   return (
     <div className="flex m-2">
       <AddProduct />
-      <ProductList />
-      <ProductDetails id={5}/>
+      <ProductList onShowDetails={handleShowDetails} />
+      <ProductDetails id={passId} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- use-react-query/src/components/ProductDetails.jsx
+++ use-react-query/src/components/ProductDetails.jsx
@@ -1,38 +1,45 @@
-import React from 'react'
-
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import React from "react";
 
-const retrieveProduct = async({queryKey}) => {
-    const response = await axios.get(`http://localhost:3000/${queryKey[0]}/${queryKey[1]}`);
-    return response.data;
-}
+const retrievedProduct = async ({ queryKey }) => {
+  const response = await axios.get(
+    `http://localhost:3000/${queryKey[0]}/${queryKey[1]}`
+  );
 
-const ProductDetails = ({id}) => {
+  return response.data;
+};
 
- const {data: product, error, isLoading } = useQuery({
+const ProductDetails = ({ id }) => {
+  const {
+    data: product,
+    error,
+    isLoading,
+  } = useQuery({
     queryKey: ["products", id],
-    queryFn: retrieveProduct,
+    queryFn: retrievedProduct,
   });
+  // console.log(product);
 
-  if (isLoading) return <div>Fetching Product Details...</div>
-  if(error) return <div>An Error Occured: {error.message}</div>
+  if (isLoading) return <div>Fetching Products...</div>;
+  if (error) return <div> An Error Ocurred:{error.message}</div>;
 
   return (
-    <div className='w-1/5'>
-        <h1 className="text-3xl my-2">Product Details</h1>
-        <div className='border bg-gray-100 p-1 text-md rounded flex flex-col'>
-            <img
-                src={product.thumbnail}
-                alt={product.title}
-                className="object-cover h-24 w-24 border rounded-full m-auto" />
-            <p>{product.title}</p>
-            <p>{product.description}</p>
-            <p>USD {product.price}</p>
-            <p>{product.rating}/5</p>
-        </div>
+    <div className="w-1/5">
+      <h1 className="text-3xl my-2">Product Details</h1>
+      <div className="border bg-violet-400 p-1 text-md rounded flex flex-col">
+        <img
+          src={product.thumbnail}
+          alt={product.title}
+          className="object-cover h-24 w-24  border rounded-full m-auto"
+        />
+        <p>{product.title}</p>
+        <p>{product.description}</p>
+        <p>USD {product.price}</p>
+        <p>{product.rating}/5</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
